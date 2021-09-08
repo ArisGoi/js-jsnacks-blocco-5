e279263def1f2488ce3800bc4e080febcdd37cc5
ArisGoi <aris.goi97@gmail.com>
2021-09-08 11:24:04 +0200

    snack-1

---
+++ snack-1/main.js
@@ -0,0 +1,73 @@
+// Crea un array di 10 oggetti che rappresentano una zucchina, indicando
+// per ognuna varietà, peso e lunghezza.
+var raccolto = [
+    {
+        "name": "zucchina",
+        "tipo": "A",
+        "peso": 200,
+        "lunghezza": 30
+    },
+    {
+        "name": "zucchina",
+        "tipo": "A",
+        "peso": 120,
+        "lunghezza": 10
+    },
+    {
+        "name": "zucchina",
+        "tipo": "A",
+        "peso": 190,
+        "lunghezza": 28
+    },
+    {
+        "name": "zucchina",
+        "tipo": "B",
+        "peso": 20,
+        "lunghezza": 10
+    },
+    {
+        "name": "zucchina",
+        "tipo": "A",
+        "peso": 250,
+        "lunghezza": 35
+    },
+    {
+        "name": "zucchina",
+        "tipo": "A",
+        "peso": 201,
+        "lunghezza": 30
+    },
+    {
+        "name": "zucchina",
+        "tipo": "A",
+        "peso": 156,
+        "lunghezza": 23
+    },
+    {
+        "name": "zucchina",
+        "tipo": "A",
+        "peso": 200,
+        "lunghezza": 30
+    },
+    {
+        "name": "zucchina",
+        "tipo": "C",
+        "peso": 133,
+        "lunghezza": 24
+    },
+    {
+        "name": "zucchina",
+        "tipo": "C",
+        "peso": 212,
+        "lunghezza": 32
+    },
+];
+
+// Calcola quanto pesano tutte le zucchine.
+var totPeso = 0;
+for (i=0; i<raccolto.length; i++){
+    totPeso += raccolto[i].peso;
+};
+
+
+console.log(`le zucchine in totale pesano ${totPeso}g`);
