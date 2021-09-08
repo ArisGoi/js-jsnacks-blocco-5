c1b08529f0b43e89498937ea3629c38018a7bca6
ArisGoi <aris.goi97@gmail.com>
2021-09-08 12:50:04 +0200

    snack-4

---
+++ snack-4/main.js
@@ -0,0 +1,14 @@
+// Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall’altro
+//es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+const array1 = ["a","b","c"];
+const array2 = [1,2,3];
+
+const arraySomma = [];
+
+for(i=0; i<array1.length; i++){
+    arraySomma.push(array1[i]);
+    arraySomma.push(array2[i]);
+};
+
+console.log(arraySomma);
