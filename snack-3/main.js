c82d9de4b26c319732b8f6a098cb506278b8abaa
ArisGoi <aris.goi97@gmail.com>
2021-09-08 12:30:09 +0200

    snack-3

---
+++ snack-3/main.js
@@ -0,0 +1,14 @@
+// Scrivi una funzione che accetti una stringa come argomento e la ritorni
+function girami(parola){
+    var inverso = "";
+
+    for (i = parola.length - 1; i >= 0; i--){
+        inverso += parola[i];
+    };
+
+    return inverso
+}
+var risultato = girami("ciao");
+
+// girata (es. Ciao -> oaiC)
+console.log(risultato);
