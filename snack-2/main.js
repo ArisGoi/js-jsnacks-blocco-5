d0e2690e592b6ea139f85ce17ad3eb325e2f99f0
ArisGoi <aris.goi97@gmail.com>
2021-09-08 11:49:36 +0200

    snack-2

---
+++ snack-2/main.js
@@ -0,0 +1,80 @@
+// Crea 10 oggetti che rappresentano una zucchina.
+var raccolto = [
+    {
+        "name": "zucchina",
+        "tipo": "A",
+        "peso": 200,
+        "lunghezza": 30
+    },
+    {
+        "name": "zucchina",
+        "tipo": "A",
+        "peso": 120,
+        "lunghezza": 10
+    },
+    {
+        "name": "zucchina",
+        "tipo": "A",
+        "peso": 190,
+        "lunghezza": 28
+    },
+    {
+        "name": "zucchina",
+        "tipo": "B",
+        "peso": 20,
+        "lunghezza": 10
+    },
+    {
+        "name": "zucchina",
+        "tipo": "A",
+        "peso": 250,
+        "lunghezza": 35
+    },
+    {
+        "name": "zucchina",
+        "tipo": "A",
+        "peso": 201,
+        "lunghezza": 30
+    },
+    {
+        "name": "zucchina",
+        "tipo": "A",
+        "peso": 156,
+        "lunghezza": 23
+    },
+    {
+        "name": "zucchina",
+        "tipo": "A",
+        "peso": 50,
+        "lunghezza": 8
+    },
+    {
+        "name": "zucchina",
+        "tipo": "C",
+        "peso": 133,
+        "lunghezza": 24
+    },
+    {
+        "name": "zucchina",
+        "tipo": "C",
+        "peso": 212,
+        "lunghezza": 32
+    },
+];
+
+// Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+var zucchinePiccole = [];
+var zucchineGrandi = [];
+
+for (i=0; i<raccolto.length; i++){
+    // totPeso += raccolto[i].peso;
+    if(raccolto[i].lunghezza < 15){
+        zucchinePiccole.push(raccolto[i]);
+    } else {
+        zucchineGrandi.push(raccolto[i]);
+    }
+};
+
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+console.log(zucchinePiccole);
+console.log(zucchineGrandi);
